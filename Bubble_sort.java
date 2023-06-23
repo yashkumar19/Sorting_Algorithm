f177b26d46a3b39911dab58bed7da947884d5987
yashkumar19 <88893297+yashkumar19@users.noreply.github.com>
2023-06-23 16:52:51 +0530

    Update Bubble_sort.java

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -16,12 +16,17 @@ class Bubble_sort{
 
     public static void sort(int[] arr, int n){
         for(int i=n-1; i>0; i--){
+            int didswap =0;
             for(int j=0; j<i; j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1]=temp;
+                    didswap=1;
                 }
+            }
+            if(didswap==0){
+                break;
             }
         }
     }
